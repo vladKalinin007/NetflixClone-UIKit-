ae550193f4e473e7438ec508233b0610c24d4334
KaliNinB <109882612+KaliNinB@users.noreply.github.com>
2022-11-24 19:52:54 +0200

    Initial Commit

---
+++ Netflix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netflix
+//
+//  Created by Владислав Калинин on 24.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
